7bab20d85391f2078e2e851dca0ef1457d2c383d
Horin <wyhorin@163.com>
2019-07-10 10:37:49 +0800

    支持中心

---
+++ src/components/AppLayout.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="app-layout">
+        <header class="header">
+            <div><img class="img" src="../assets/logo.svg" /></div>
+            <div>My shirt shop</div>
+        </header>
+
+        <!-- 导航菜单 -->
+        <NavMenu />
+
+        <!-- 菜单放在这 -->
+        <router-view />
+    </div>
+</template>
+
+<script>
+import NavMenu from './NavMenu.vue'
+
+export default {
+    components: {
+        NavMenu,
+    }
+}
+</script>
+
+<style lang="stylus">
+@import '../style/main';
+</style>
+
+<style lang="stylus" scoped>
+.header {
+    .img {
+        width: 64px;
+        height: 64px;
+    }
+}
+</style>
