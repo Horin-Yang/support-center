fee609dccf5d0d5a1e512e661b29824c628d132d
Horin <wyhorin@163.com>
2019-07-15 17:48:25 +0800

    表单输入组件 自定义v-model

---
+++ src/components/FormInput.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="row">
+    <input
+      class="input"
+      :class="inputClass"
+      :name="name"
+      :type="type"
+      :value="text"
+      :placeholder="placeholder"
+      @input="update"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+    },
+    type: {
+      type: String,
+      default: 'text',
+    },
+    // value: {
+    text: {
+      required: true,
+    },
+    placeholder: {
+      type: String,
+    },
+    invalid: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  computed: {
+    inputClass () {
+      return {
+        'invalid': this.invalid,
+      }
+    }
+  },
+
+  methods: {
+    update (event) {
+      // console.log(event.currentTarget.value)
+      // this.$emit('input', event.currentTarget.value)
+      this.$emit('update', event.currentTarget.value)
+    },
+  },
+
+  model: {
+    prop: 'text',
+    event: 'update',
+  }
+}
+</script>
+
